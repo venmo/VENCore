1de386f5752ec6a34c5197651baf884a971dadc2
Ben Guo <ben@venmo.com>
2014-04-22 15:14:59 -0400

    Add access token retrieval to PaymentSandboxSpec

--- VENCoreIntegrationTests/PaymentSandboxSpec.m
+++ VENCoreIntegrationTests/PaymentSandboxSpec.m
@@ -1,4 +1,13 @@
+//
+// To run these tests, you'll need to add a "config.plist" file to the VENCoreIntegrationTests target.
+// This plist should contain an "access_token" key with your Venmo access token as the value.
+//
+
 SpecBegin(PaymentSandbox)
+
+NSString *plistPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"config" ofType:@"plist"];
+NSDictionary *config = [[NSDictionary alloc] initWithContentsOfFile:plistPath];
+NSString *accessToken = config[@"access_token"];
 
 describe(@"Settled Payment", ^{
 
